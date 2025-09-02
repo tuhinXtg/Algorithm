e51acf532d628c012862e9628d64026742c3cfc7
tuhinXtg <natuhin02@gmail.com>
2025-09-02 18:13:10 +0600

    bfs

---
+++ Module 2/BFS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[1005];
+bool vis[1005];
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        cout << par << " ";
+        for (int child : adj_list[par])
+        {
+            if(vis[child] == false)
+            {
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
+int main() {
+    int n,e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis,false,sizeof(vis));
+    bfs(1);
+    return 0;
+}
