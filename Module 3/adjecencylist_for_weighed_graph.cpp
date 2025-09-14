a908648483e334880314eb882f3f26cc9f6d063e
tuhinXtg <natuhin02@gmail.com>
2025-09-14 12:26:53 +0600

    dijkstra algorithm

---
+++ Module 3/adjecencylist_for_weighed_graph.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,e;
+    cin >> n >> e;
+    vector<pair<int,int>> adj_lst[n];
+    while (e--)
+    {
+        int a,b,c;
+        cin >> a >> b >> c;
+        adj_lst[a].push_back({b,c});
+        adj_lst[b].push_back({a,c});
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " -> ";
+        for (pair<int,int> p : adj_lst[i])
+        {
+            cout << p.first <<  " " << p.second << ", ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
