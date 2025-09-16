f9a65294957d0b91acb18d88c01143f9660d09b8
tuhinXtg <natuhin02@gmail.com>
2025-09-16 16:47:40 +0600

    Detect negative weighted cycle using Bellman-Ford

--- Module 9/detect_negative_weighed_cycleusing_bellma_ford.cpp
+++ Module 9/detect_negative_weighed_cycleusing_bellma_ford.cpp
@@ -30,17 +30,28 @@ void bellman_ford()
             }
         }
     }
+    bool cycle = false;
     for(auto ed : edge_lst)
+    {
+        int a,b,c;
+        a = ed.a;
+        b = ed.b;
+        c = ed.c;
+        if(dis[a]!=INT_MAX && dis[a]+c<dis[b])
         {
-            int a,b,c;
-            a = ed.a;
-            b = ed.b;
-            c = ed.c;
-            if(dis[a]!=INT_MAX && dis[a]+c<dis[b])
-            {
-                dis[b] = dis[a]+c;
-            }
+            cycle = true;
+            break;
         }
+    }
+    if(cycle)
+        cout << "Negative weighed cycle detected" << endl;
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << i << " -> " << dis[i] << endl;
+        }
+    }
 }
 int main() {
     cin >> n >> e;
@@ -56,10 +67,5 @@ int main() {
     }
     dis[0] = 0;
     bellman_ford();
-    for (int i = 0; i < n; i++)
-    {
-        cout << i << " -> " << dis[i] << endl;
-    }
-    
     return 0;
 }
